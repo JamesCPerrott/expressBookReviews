b1ad54bdacfe936d15909a5d1f86070c8f18c141
JamesCPerrott <jamescperrott@yahoo.com>
2025-02-14 15:24:49 -0500

    updated db

--- final_project/index.js
+++ final_project/index.js
@@ -11,7 +11,6 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-    //Write the authenication mechanism here
     // Check if user is authenticated
     if (req.session.authorization) {
         let token = req.session.authorization['accessToken']; // Access Token
@@ -37,4 +36,4 @@ const PORT =5000;
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT,()=>console.log(`Server is running on port: `+PORT));
--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,14 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
+      1: {"author": "Chinua Achebe","title": "Things Fall Apart","ISBN": "9780385474542", "reviews": {} },
+      2: {"author": "Hans Christian Andersen","title": "Fairy tales","ISBN": "9780141329017", "reviews": {} },
+      3: {"author": "Dante Alighieri","title": "The Divine Comedy","ISBN": "9780451208637", "reviews": {} },
+      4: {"author": "Unknown","title": "The Epic Of Gilgamesh","ISBN": "9780140441000", "reviews": {} },
+      5: {"author": "Unknown","title": "The Book Of Job","ISBN": "9781433546471", "reviews": {} },
+      6: {"author": "Unknown","title": "One Thousand and One Nights","ISBN": "9780140442892", "reviews": {} },
+      7: {"author": "Unknown","title": "Nj\u00e1l's Saga","ISBN":"9780140447699", "reviews": {} },
+      8: {"author": "Jane Austen","title": "Pride and Prejudice","ISBN":"9780141439518", "reviews": {} },
+      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot","ISBN": "9780199538751", "reviews": {} },
+      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy","ISBN": "9780375400704", "reviews": {} }
 }
 
 module.exports=books;
